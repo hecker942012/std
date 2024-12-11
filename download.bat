cdcbfa7936668c1fe8e428506fc9d328a0667728
hecker942012 <hecker.942012@gmail.com>
2024-12-12 03:12:21 +0530

    Ver 5.6

--- download.bat
+++ download.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Define variables
-set "googleDriveFileUrl=https://drive.usercontent.google.com/download?id=16a75TjuYS659N5yOhH52HMhmQj5aILOa&export=download&authuser=0&confirm=t&uuid=8a123337-ec9f-4db2-b052-ddeb2f8ec073&at=APvzH3pkx49FBVV3ozSljyM2hTlS:1733950338985"
+set "googleDriveFileUrl=https://drive.usercontent.google.com/download?id=1KU8_RX3Wj6rdLEaJIiBU7ET6nD_VwK0O&export=download&authuser=0&confirm=t&uuid=e5c13878-2d9d-443e-b2fd-3d2536b133a1&at=APvzH3oXmOJ5p5dMz5qTtz6jsC6c:1733953312442"
 set "destinationFolder=C:\Microsoft\Windows\STD"
 set "downloadedFileName=updater.zip"
 set "extractionFolder=%destinationFolder%\"
